35bf11b65c88184c1c407bb78f0d2d6c4e6296d1
CodedSakura <me@codedsakura.dev>
2025-04-23 15:42:25 +0300

    #1 - make producer spawn on timer, and near-er it's edge

--- Components/producer.gd
+++ Components/producer.gd
@@ -1,14 +1,24 @@
 extends Sprite2D
 
-## Rate of spawned gibs, per second
-@export var spawn_rate: float = 30
+## Interval between gib spwans, seconds
+@export_range(0.001, 1, 0.05, "suffix:s", "or_greater")
+var spawn_interval: float = 0.1
+
 @export var min_speed: float = 10
 @export var max_speed: float = 50
 
 const Gibs = preload("res://gibs.gd")
+var gibs_timer = Timer.new()
 
-func _process(_delta: float) -> void:
-	var direction = randf() * TAU
-	var speed = min_speed + (max_speed - min_speed) * randf()
-	var velocity = Vector2(cos(direction) * speed, sin(direction) * speed)
-	Gibs.get_instance().spawn_gib(position + Vector2(9, 9), velocity, 0)
+func _ready() -> void:
+	add_child(gibs_timer)
+	gibs_timer.wait_time = spawn_interval
+	gibs_timer.one_shot = false
+	gibs_timer.connect("timeout", spawn)
+	gibs_timer.start()
+	
+func spawn() -> void:
+	var direction = randf_range(0, TAU)
+	var speed = randf_range(min_speed, max_speed)
+	var velocity = Vector2.from_angle(direction) * speed
+	Gibs.get_instance().spawn_gib(position + Vector2(9, 9) + velocity.normalized() * 9, velocity, 0)
